7595b3baa386d9189890cd4bd389ee5f5b671f5a
jrxmae <leah.mullin9@gmail.com>
2024-04-17 16:59:38 +0000

    Add request for input from user for name of month and sale target amount

--- run.py
+++ run.py
@@ -10,3 +10,9 @@ def welcome():
 Calls welcome function to print the welcome message to the user
 '''
 welcome()
+
+'''
+Asks user for name of the month and the sale target amount
+'''
+month = input('Enter the name of the month: ')
+monthly_target = int(input("Enter the sale target of the month: "))
