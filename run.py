3f21f77e653364c597957f4e919be285fee5d16d
jrxmae <leah.mullin9@gmail.com>
2024-04-17 17:13:25 +0000

    Add print function that compares the today's sale to monthly target

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ A function made up of a welcome message for the user
 '''
 def welcome():
     print('Welcome to Sale Target Tracker!')
-    print('With this program, you can input your daily sales and compare it to your monthly target')
+    print('With this program, you can input your daily sales and compare it to your monthly target.')
 
 
 '''
@@ -22,3 +22,8 @@ Asks user for the date and the sale amount made on the date inputted
 '''
 date = int(input("Enter today's date as ddmmyy: "))
 today_sale = int(input('Enter the sale amount made today: '))
+
+'''
+Compares the sale amount made on the day inputted to the sale target for the month
+'''
+print("You have made", today_sale, "out of", monthly_target)
