96d05408cd777efc064ef42b76c03749ea2859a0
jrxmae <leah.mullin9@gmail.com>
2024-04-19 18:04:07 +0000

    Add a while loop for input validation for month

--- run.py
+++ run.py
@@ -1,30 +1,46 @@
-'''
-A function made up of a welcome message for the user
-'''
+
 def welcome():
+    '''
+    A function made up of a welcome message for the user
+    '''
     print('Welcome to Sale Target Tracker!')
     print('With this program, you can input your daily sales and compare it to your monthly target.')
 
+def user_input():
+    '''
+    Asks user for name of the month and the sale target amount with input validation for both
+    '''
+    month = input('Enter the name of the month: ')
+    while month.lower() not in [
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december"
+    ]:
+        print("Invalid month.")
+        month = input('Enter the name of the month: ')
+    monthly_target = input("Enter the sale target of the month: ")
+    '''
+    Asks user for the date and the sale amount made on the date inputted
+    '''
+    date = int(input("Enter today's date as ddmmyy: "))
+    today_sale = int(input('Enter the sale amount made today: '))
 
-'''
-Calls welcome function to print the welcome message to the user
-'''
-welcome()
+def compare():
+    '''
+    Compares the sale amount made on the day inputted to the sale target for the month and prints depending on if the target was reached or not
+    '''
+    if today_sale < monthly_target:
+        print("There is", monthly_target - today_sale, "left to make. Goodluck!")
+    else:
+        print("Congratulations! You passed the target by", today_sale - monthly_target)
+        
 
-'''
-Asks user for name of the month and the sale target amount
-'''
-month = input('Enter the name of the month: ')
-monthly_target = int(input("Enter the sale target of the month: "))
-
-'''
-Asks user for the date and the sale amount made on the date inputted
-'''
-date = int(input("Enter today's date as ddmmyy: "))
-today_sale = int(input('Enter the sale amount made today: '))
-
-'''
-Compares the sale amount made on the day inputted to the sale target for the month
-'''
-print("You have made", today_sale, "out of", monthly_target)
-print("There is", monthly_target - today_sale, "left to make. Goodluck!")
